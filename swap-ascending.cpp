7e751a253672f67f5225fcdddc634a1b66c0f702
Tung Dinh Quang <117490120+qtungnui07@users.noreply.github.com>
2024-07-18 21:47:21 +0700

    Create swap-ascending.cpp

---
+++ swap-ascending.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void swap(int &x, int &y){
+	int temp =x;
+	x = y;
+	y=temp;}
+int main(){
+	int a,b,c;
+	cin>>a>>b>>c;
+	if (a>b) swap(a,b);
+	if (a>c) swap(a,c);
+	if (b>c) swap(b,c);
+	cout<<a<<" "<<b<<" "<<c<<" "<<endl;
+	return 0;}
